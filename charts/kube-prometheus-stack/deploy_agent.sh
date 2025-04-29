615f088b6916f812ef0acd3a2d76ac09193ad23c
liangzhaoliang <zhaoliang.liang@plaso.me>
2025-04-29 23:01:26 +0800

    feat: 优化

--- charts/kube-prometheus-stack/deploy_agent.sh
+++ charts/kube-prometheus-stack/deploy_agent.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+
+storageClass=$1
+clusterLabel=$2
+
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 echo "prometheus-community repo added and updated"
@@ -8,15 +12,14 @@ cp values_agent.yaml values_temp.yaml
 # 将nfs-prom替换为nfs-temp
 # 判断如果是macos系统，则使用gsed命令
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  sed -i "" 's/P_STORAGE_CLASS_P/nfs-prom/g' values_temp.yaml
+  sed -i "" "s/P_STORAGE_CLASS_P/$storageClass/g" values_temp.yaml
   # 调整全局标签集群名称
-  sed -i "" 's/P_LABEL_CLUSTER_P/rongke-private/g' values_temp.yaml
+  sed -i "" "s/P_LABEL_CLUSTER_P/$clusterLabel/g" values_temp.yaml
 else
-  sed -i 's/P_STORAGE_CLASS_P/nfs-prom/g' values_temp.yaml
+  sed -i "s/P_STORAGE_CLASS_P/$storageClass/g" values_temp.yaml
   # 调整全局标签集群名称
-  sed -i 's/P_LABEL_CLUSTER_P/rongke-private/g' values_temp.yaml
+  sed -i "s/P_LABEL_CLUSTER_P/$clusterLabel/g" values_temp.yaml
 fi
-
 
 echo "start installing prometheus agent"
 
@@ -24,4 +27,6 @@ helm upgrade --install rongke-prometheus prometheus-community/kube-prometheus-st
 
 echo "prometheus agent installed"
 
-#rm -rf values_temp.yaml
+rm -rf values_temp.yaml
+
+kubectl apply -f ./secrets/prometheus_remote_secrets.yaml -n monitoring
