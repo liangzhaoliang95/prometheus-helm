eb1b73665df8b03d08febd7c8cc0f37de0a68c32
liangzhaoliang <zhaoliang.liang@plaso.me>
2025-04-29 23:01:26 +0800

    feat: 优化

--- charts/kube-prometheus-stack/deploy_server.sh
+++ charts/kube-prometheus-stack/deploy_server.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+storageClass=$1
+clusterLabel=$2
+
 helm repo add prometheus-community https://helm.geekz.cn:81/repository/helm-proxy
 helm repo update
 echo "prometheus-community repo added and updated"
@@ -9,13 +12,13 @@ cp values_server.yaml values_temp.yaml
 # 将nfs-prom替换为nfs-temp
 # 判断如果是macos系统，则使用gsed命令
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  sed -i "" 's/P_STORAGE_CLASS_P/nfs-temp/g' values_temp.yaml
+  sed -i "" "s/P_STORAGE_CLASS_P/$storageClass/g" values_temp.yaml
   # 调整全局标签集群名称
-  sed -i "" 's/P_LABEL_CLUSTER_P/lxz/g' values_temp.yaml
+  sed -i "" "s/P_LABEL_CLUSTER_P/$clusterLabel/g" values_temp.yaml
 else
-  sed -i 's/P_STORAGE_CLASS_P/nfs-temp/g' values_temp.yaml
+  sed -i "s/P_STORAGE_CLASS_P/$storageClass/g" values_temp.yaml
   # 调整全局标签集群名称
-  sed -i 's/P_LABEL_CLUSTER_P/lxz/g' values_temp.yaml
+  sed -i "s/P_LABEL_CLUSTER_P/$clusterLabel/g" values_temp.yaml
 fi
 
 
@@ -26,6 +29,8 @@ helm upgrade --install rongke-prometheus prometheus-community/kube-prometheus-st
 echo "prometheus stack installed"
 
 rm -rf values_temp.yaml
+
+kubectl apply -f ./secrets/prometheus_remote_secrets.yaml -n monitoring
 
 # 对中心集群的服务监控进行修改 添加全局cluster标签
 kubectl get servicemonitor -A -o yaml | \
