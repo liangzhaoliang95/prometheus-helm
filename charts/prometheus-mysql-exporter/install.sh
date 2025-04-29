ac18d6abb68e06e72f486d7a2c1db8ba6f10c46f
liangzhaoliang <liangzhaoliang95@163.com>
2025-04-29 21:02:45 +0800

    feat: 调整prometheus默认配置

--- charts/prometheus-mysql-exporter/install.sh
+++ charts/prometheus-mysql-exporter/install.sh
@@ -1,5 +1,14 @@
 #!/bin/zsh
+
+cp values.yaml values_temp.yaml
+
+sed -i 's/P_LABEL_CLUSTER_P/rongke-private/g' values_temp.yaml
+sed -i 's/P_MYSQL_HOST_P/172.17.3.189/g' values_temp.yaml
+sed -i 's/P_MYSQL_PASSWORD_P/11#Plaso1865(*/g' values_temp.yaml
+
 helm upgrade --install rongke-mysql-exporter prometheus-community/prometheus-mysql-exporter \
   --namespace monitoring \
   --create-namespace \
-  -f values.yaml
+  -f values_temp.yaml
+
+rm -rf values_temp.yaml
