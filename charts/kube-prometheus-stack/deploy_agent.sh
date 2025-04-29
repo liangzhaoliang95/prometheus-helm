c3f9b5d53129621a63d66a0443ea27218e078741
liangzhaoliang <liangzhaoliang95@163.com>
2025-04-29 21:02:45 +0800

    feat: 调整prometheus默认配置

--- charts/kube-prometheus-stack/deploy_agent.sh
+++ charts/kube-prometheus-stack/deploy_agent.sh
@@ -8,13 +8,13 @@ cp values_agent.yaml values_temp.yaml
 # 将nfs-prom替换为nfs-temp
 # 判断如果是macos系统，则使用gsed命令
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  sed -i "" 's/nfs-prom/nfs-prom/g' values_temp.yaml
+  sed -i "" 's/P_STORAGE_CLASS_P/nfs-prom/g' values_temp.yaml
   # 调整全局标签集群名称
-  sed -i "" 's/lxz/rongke-private/g' values_temp.yaml
+  sed -i "" 's/P_LABEL_CLUSTER_P/rongke-private/g' values_temp.yaml
 else
-  sed -i 's/nfs-prom/nfs-prom/g' values_temp.yaml
+  sed -i 's/P_STORAGE_CLASS_P/nfs-prom/g' values_temp.yaml
   # 调整全局标签集群名称
-  sed -i 's/lxz/rongke-private/g' values_temp.yaml
+  sed -i 's/P_LABEL_CLUSTER_P/rongke-private/g' values_temp.yaml
 fi
 
 
