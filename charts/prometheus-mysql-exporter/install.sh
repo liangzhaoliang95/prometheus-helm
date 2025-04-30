13be50f20cb7751cf1a6e9b08cb728985099915d
liangzhaoliang <liangzhaoliang95@163.com>
2025-04-30 16:14:07 +0800

    feat:

--- charts/prometheus-mysql-exporter/install.sh
+++ charts/prometheus-mysql-exporter/install.sh
@@ -2,9 +2,9 @@
 
 cp values.yaml values_temp.yaml
 
-sed -i 's/P_LABEL_CLUSTER_P/rongke-private/g' values_temp.yaml
-sed -i 's/P_MYSQL_HOST_P/172.17.3.189/g' values_temp.yaml
-sed -i 's/P_MYSQL_PASSWORD_P/11#Plaso1865(*/g' values_temp.yaml
+sed -i 's/P_LABEL_CLUSTER_P/lxz/g' values_temp.yaml
+sed -i 's/P_MYSQL_HOST_P/192.168.10.2/g' values_temp.yaml
+sed -i 's/P_MYSQL_PASSWORD_P/xxx/g' values_temp.yaml
 
 helm upgrade --install rongke-mysql-exporter prometheus-community/prometheus-mysql-exporter \
   --namespace monitoring \
