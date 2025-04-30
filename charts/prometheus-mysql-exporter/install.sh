9790bf08e4c8c3fd058d4da68b007d96c1b6b1e9
liangzhaoliang <zhaoliang.liang@plaso.me>
2025-04-30 22:29:12 +0800

    feat: 优化

--- charts/prometheus-mysql-exporter/install.sh
+++ charts/prometheus-mysql-exporter/install.sh
@@ -3,8 +3,10 @@
 cp values.yaml values_temp.yaml
 
 sed -i 's/P_LABEL_CLUSTER_P/lxz/g' values_temp.yaml
-sed -i 's/P_MYSQL_HOST_P/192.168.10.2/g' values_temp.yaml
-sed -i 's/P_MYSQL_PASSWORD_P/xxx/g' values_temp.yaml
+sed -i 's/P_MYSQL_HOST_P/192.168.0.228/g' values_temp.yaml
+sed -i 's/P_MYSQL_USER_P/root/g' values_temp.yaml
+sed -i 's/P_MYSQL_PASSWORD_P/gXbg01N^ixRm%*6/g' values_temp.yaml
+sed -i 's/P_MYSQL_PORT_P/13049/g' values_temp.yaml
 
 helm upgrade --install rongke-mysql-exporter prometheus-community/prometheus-mysql-exporter \
   --namespace monitoring \
