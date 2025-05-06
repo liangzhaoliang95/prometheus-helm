8fa1d4542d8e62a40ec8a10c4ee2a3a003286e48
liangzhaoliang <zhaoliang.liang@plaso.me>
2025-05-06 11:21:27 +0800

    feat: 新增Node-export安装

---
+++ charts/kube-prometheus-stack/node-exporter/init_node_exporter.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+docker run -d \
+  --name=node-exporter \
+  --net=host \
+  --pid=host \
+  --privileged \
+  -v "/:/host:ro,rslave" \
+  -v "/sys:/host/sys:ro" \
+  -v "/proc:/host/proc:ro" \
+  docker-proxy-quay.geekz.cn:81/prometheus/node-exporter \
+  --path.rootfs=/host
