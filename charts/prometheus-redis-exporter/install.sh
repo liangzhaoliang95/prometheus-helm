e63b8eb4177bff6457fa0006f7adf2e08fad22ae
liangzhaoliang <liangzhaoliang95@163.com>
2025-05-07 18:14:06 +0800

    feat: agent端禁用存储

--- charts/prometheus-redis-exporter/install.sh
+++ charts/prometheus-redis-exporter/install.sh
@@ -3,8 +3,8 @@
 cp values.yaml values_temp.yaml
 
 sed -i 's/P_LABEL_CLUSTER_P/rongke-private/g' values_temp.yaml
-sed -i 's/P_REDIS_HOST_P/172.17.3.189/g' values_temp.yaml
-sed -i 's/P_REDIS_PASSWORD_P/11#Plaso1865(*/g' values_temp.yaml
+sed -i 's/P_REDIS_HOST_P/172.17.3.213/g' values_temp.yaml
+sed -i 's/P_REDIS_PASSWORD_P/kVf1Zqf1vf8IN8M/g' values_temp.yaml
 
 helm upgrade --install rongke-redis-exporter prometheus-community/prometheus-redis-exporter \
   --namespace monitoring \
