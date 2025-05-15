499c7b5f4b42a9f0894742cb78622422accac52c
liangzhaoliang <liangzhaoliang95@163.com>
2025-05-15 22:59:00 +0800

    feat: 优化发布脚本

--- charts/kube-prometheus-stack/deploy_server.sh
+++ charts/kube-prometheus-stack/deploy_server.sh
@@ -13,13 +13,13 @@ cp values_server.yaml values_temp.yaml
 # 将nfs-prom替换为nfs-temp
 # 判断如果是macos系统，则使用gsed命令
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  sed -i "" "s/P_STORAGE_CLASS_P/$storageClass/g" values_temp.yaml
+  sed -i "" "s|P_STORAGE_CLASS_P|$storageClass|g" values_temp.yaml
   # 调整全局标签集群名称
-  sed -i "" "s/P_LABEL_CLUSTER_P/$clusterLabel/g" values_temp.yaml
+  sed -i "" "s|P_LABEL_CLUSTER_P|$clusterLabel|g" values_temp.yaml
 else
-  sed -i "s/P_STORAGE_CLASS_P/$storageClass/g" values_temp.yaml
+  sed -i "s|P_STORAGE_CLASS_P|$storageClass|g" values_temp.yaml
   # 调整全局标签集群名称
-  sed -i "s/P_LABEL_CLUSTER_P/$clusterLabel/g" values_temp.yaml
+  sed -i "s|P_LABEL_CLUSTER_P|$clusterLabel|g" values_temp.yaml
 fi
 
 
--- charts/prometheus-mysql-exporter/install.sh
+++ charts/prometheus-mysql-exporter/install.sh
@@ -2,11 +2,11 @@
 
 cp values.yaml values_temp.yaml
 
-sed -i 's/P_LABEL_CLUSTER_P/lxz/g' values_temp.yaml
-sed -i 's/P_MYSQL_HOST_P/192.168.0.228/g' values_temp.yaml
+sed -i 's/P_LABEL_CLUSTER_P/rongke-private/g' values_temp.yaml
+sed -i 's/P_MYSQL_HOST_P/172.17.3.189/g' values_temp.yaml
 sed -i 's/P_MYSQL_USER_P/root/g' values_temp.yaml
-sed -i 's/P_MYSQL_PASSWORD_P/gXbg01N^ixRm%*6/g' values_temp.yaml
-sed -i 's/P_MYSQL_PORT_P/13049/g' values_temp.yaml
+sed -i 's/P_MYSQL_PASSWORD_P/11#asd(*/g' values_temp.yaml
+sed -i 's/P_MYSQL_PORT_P/3306/g' values_temp.yaml
 
 helm upgrade --install rongke-mysql-exporter prometheus-community/prometheus-mysql-exporter \
   --namespace monitoring \
